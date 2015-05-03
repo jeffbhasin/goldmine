3ed69fc34c4181837207adc9ea6cd3e33f7d9a5f
Jeffrey Bhasin <jeffb@case.edu>
2015-05-03 18:48:20 -0400

    Have drawGenomePool() warn if restricting chrs automatically

--- R/enrichment.r
+++ R/enrichment.r
@@ -83,15 +83,17 @@ testEnrichment <- function(query, null, features)
 #' Given a query set of ranges, draw a length-matched pool of sequences. Returned ranges are required to (1) not overlap with each other or the query, (2) not extend off chromosome ends, (3) not extend over assembly gaps as defined in the UCSC "gap" table for the given genome assembly.
 #' @param query A data.frame or data.table with columns "chr", "start", and "end" and any other columns. If a data.frame or data.table, must contain the columns "chr", "start", "end", where the "start" coordinates are 1-based.
 #' @param n Number of times greater than the query set that the size of the returned background pool will be
+#' @param chrs Vector of chromosome names to draw from. Useful for restricting to canonical chromosomes only, i.e. chr1 to chr22, chrX, and chrY for hg19. If not given, will restrict to the chromosome names present in query.
 #' @param genome The UCSC name specific to the genome of the query coordinates (e.g. "hg19", "hg18", "mm10", etc)
 #' @param cachedir A path to a directory where a local cache of UCSC tables are stored. If equal to \code{NULL} (default), the data will be downloaded to temporary files and loaded on the fly. Caching is highly recommended to save time and bandwidth.
 #' @return A GRanges of the background sequences.
 #' @export
-drawGenomePool <- function(query, n, genome, cachedir, chrs=NULL)
+drawGenomePool <- function(query, n, chrs=NULL, genome, cachedir)
 {
 	target.gr <- makeGRanges(query)
 	if(is.null(chrs))
 	{
+		message("Drawing from chrs: ",toString(seqlevels(target.gr))," because these were present in query. Please provide the \"chrs\" option if you wish to draw from additional chromosomes.")
 		chrs <- unique(seqnames(target.gr))
 	}
 
